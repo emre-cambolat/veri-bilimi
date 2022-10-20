f77b3227610a7ece473ca0d6492fd098c30156c1
emre-cambolat <cambolatemre@outlook.com>
2022-10-20 21:16:33 +0300

    first-commit

---
+++ odev-1.py
@@ -0,0 +1,65 @@
+
+
+class Calisan:
+    def __init__(self, _tcKimlikNo, _ad, _soyad, _yas, _maas, _calismaSaati):
+        self.tcKimlikNo = int(_tcKimlikNo)
+        self.ad = str(_ad)
+        self.soyad = str(_soyad)
+        self.yas = int(_yas)
+        self.maas = float(_maas)
+        self.calismaSaati = int(_calismaSaati)
+
+    vergiMiktari = 0
+    vergisizMaas = 0
+
+    def vergiHesapla(self):
+        global vergiMiktari
+        if self.maas < 8000:
+            vergiMiktari = self.maas * 0.12
+        else:
+            vergiMiktari = self.maas * 0.18
+        # print('Vergi Miktarı: ' + str(vergiMiktari))
+
+    def sonDurumMaas(self):
+        global vergisizMaas
+        vergisizMaas = self.maas - vergiMiktari
+        # print("Son Durum Maaş: " + str(vergisizMaas))
+
+    def goster(self):
+        print("TC: ", self.tcKimlikNo, ", Ad Soyad: ", self.ad, " ", self.soyad, ", Yaş: ",
+              self.yas, ", Maaş: ", self.maas, ", Çalışma Saati: ", self.calismaSaati, ", Vergisiz Maaş: ", self.vergisizMaas)
+
+
+class PartTimeCalisan(Calisan):
+    def __init__(self, _tcKimlikNo, _ad, _soyad, _yas, _saatlikUcret, _calismaSaati):
+        super().__init__(_tcKimlikNo, _ad, _soyad, _yas,_saatlikUcret , _calismaSaati)
+        self.saatlikUcret = int(_saatlikUcret)
+
+    def sonDurumMaas(self):
+        self.maas = self.saatlikUcret * self.calismaSaati
+        super().sonDurumMaas()
+
+
+class FullTimeCalisan(Calisan):
+    def __init__(self, _tcKimlikNo, _ad, _soyad, _yas, _maas):
+        super().__init__(_tcKimlikNo, _ad, _soyad, _yas, _maas)
+
+    def goster(self):
+        print(self.tcKimlikNo, " ", self.ad, " ",
+              self.soyad, " ", self.yas, " ", self.maas)
+
+        print("TC: ", self.tcKimlikNo, ", Ad Soyad: ", self.ad, " ", self.soyad, ", Yaş: ",
+              self.yas, ", Maaş: ", self.maas, ", Vergisiz Maaş: ", self.vergisizMaas)
+
+    def sonDurumMaas(self):
+        super().sonDurumMaas()
+        self.vergisizMaas = self.vergisizMaas - self.vergisizMaas * 0.08
+        # print("Son Durum Maaş: ", str(self.vergisizMaas))
+
+
+partTimeCalisan = PartTimeCalisan(56798776723, "Emre", "Cambolat", 23, 100, 32)
+# fullTimeCalisan = FullTimeCalisan(23478075633, "Veli", "Bacik", 27, 9000)
+
+partTimeCalisan.vergiHesapla()
+partTimeCalisan.sonDurumMaas()
+partTimeCalisan.goster()
